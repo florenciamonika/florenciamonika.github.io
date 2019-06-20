c1ed6a239c775f9e042320f85702c66a6d85c753
monikayuliana <florenciamonika@gmail.com>
2019-06-21 03:01:50 +0700

    fractions build

---
+++ fractions/precache-manifest.cc76f5931bd42d3dbd954e1cafac250d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d2efe1b359911f4d76d46c5a7129c82",
+    "url": "/fractions/index.html"
+  },
+  {
+    "revision": "919359a540962491eed8",
+    "url": "/fractions/static/css/main.67143968.chunk.css"
+  },
+  {
+    "revision": "75a697f06f12e2ffd85c",
+    "url": "/fractions/static/js/2.8ecfd2d1.chunk.js"
+  },
+  {
+    "revision": "919359a540962491eed8",
+    "url": "/fractions/static/js/main.283e086a.chunk.js"
+  },
+  {
+    "revision": "1e52aeb8e6bd0a415582",
+    "url": "/fractions/static/js/runtime~main.1370d3e3.js"
+  },
+  {
+    "revision": "592dffb1513d98658ad60899d0f17c81",
+    "url": "/fractions/static/media/tokopedia.592dffb1.png"
+  }
+]);
